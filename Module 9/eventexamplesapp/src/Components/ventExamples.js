889a96003ac37de2f4ca82e2e0d66b63751fc5aa
Mounisha-tech <mounisha.v.21@gmail.com>
2026-07-26 23:09:14 +0530

    Completed React Hands-on - Event Handling and Currency Convertor

---
+++ Module 9/eventexamplesapp/src/Components/ventExamples.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+
+class EventExamples extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            count: 0
+        };
+    }
+
+    increment = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    };
+
+    decrement = () => {
+        this.setState({
+            count: this.state.count - 1
+        });
+    };
+
+    sayHello = () => {
+        alert("Hello! Have a nice day.");
+    };
+
+    handleIncrement = () => {
+        this.increment();
+        this.sayHello();
+    };
+
+    sayWelcome = (msg) => {
+        alert(msg);
+    };
+
+    onPress = () => {
+        alert("I was clicked");
+    };
+
+    render() {
+
+        return (
+
+            <div>
+
+                <h2>Counter : {this.state.count}</h2>
+
+                <button onClick={this.handleIncrement}>
+                    Increment
+                </button>
+
+                <button onClick={this.decrement}>
+                    Decrement
+                </button>
+
+                <br /><br />
+
+                <button
+                    onClick={() => this.sayWelcome("Welcome")}
+                >
+                    Say Welcome
+                </button>
+
+                <br /><br />
+
+                <button onClick={this.onPress}>
+                    OnPress
+                </button>
+
+            </div>
+
+        );
+
+    }
+
+}
+
+export default EventExamples;
